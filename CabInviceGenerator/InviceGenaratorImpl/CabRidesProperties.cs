ac8cf7d75e49d8961fd89b3a5684ea968a92a95d
cchoudhari1@gmail.com <cchoudhari1@gmail.com>
2020-03-31 21:02:11 +0530

    [Chetan] Add. added implementation of Cab Invice Generator of step 2: given Multiple ride data and Invice generator should return total fare

---
+++ CabInviceGenerator/InviceGenaratorImpl/CabRidesProperties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InviceGenaratorImpl
+{
+    public class CabRidesProperties
+    {
+        
+        public CabRidesProperties(double kms,double timeInMinutes)
+        {
+            Kms = kms;
+            TimeInMinutes = timeInMinutes;
+        }
+
+        public double Kms { get; }
+        public double TimeInMinutes { get; }
+    }
+}
